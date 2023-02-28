d57cea0359e187b83888b118bc057c4f5222aafd
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2023-02-28 20:29:27 +0200

    ch(appsetup) : proj setup
    -fixed port number,json returns
    
    [WIP ch-appsetup]

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-import swaggerDocs from "./api-docs/swagger";
+import swaggerDocs from './api-docs/swagger';
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin:"*"}));
+app.use(cors({ origin: '*' }));
 dotenv.config();
 
-const port = process.env.PORT ? process.env.PORT : 501
+const port = process.env.PORT ? process.env.PORT : 3000;
 
-app.listen(port,() =>{
-    console.log(`Server is running on http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 swaggerDocs(app);
 
-app.use("/",(req,res) =>{
-    res.send('<h2>Welcome to our Ecommerce App</h2>')
-})
+app.get('/', (_, res) => {
+  res.status(200).json("Welcome to our Ecommerce App");
+});
