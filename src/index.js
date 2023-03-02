bb296ff24d5e26bc1f44ef6416b963075634bf14
chrill-christella <ufiteyezuchristella@gmail.com>
2023-03-02 17:20:40 +0200

    setup support languages: -install dependencies in package.json -created a folder for languages [finish]

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes/index";
 
-import swaggerDocs from "./api-docs/swagger";
+import languages from './locales/languages';
+import swaggerDocs from './api-docs/swagger';
+import connectDb from "./database/connectDb";
 
 const app = express();
 
@@ -10,6 +12,7 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 swaggerDocs(app);
+languages(app);
 
 app.get("/", (_, res) => {
   res.status(200).json("Welcome to our Ecommerce App");
