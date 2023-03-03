67c672d17c515482ab2b3dc4e8d290b805edf6df
Shumba250 <mudakikwaaimable05@gmail.com>
2023-03-03 11:04:23 +0200

    ch(setup tests): a sample test case passing automatically changes to test database when running tests setup a test environment test coverage badge added to a readme.md file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import routes from "./routes/index";
 
 import swaggerDocs from "./api-docs/swagger";
-
-import connectDb from "./database/connectDb";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-dotenv.config();
-
-const port = process.env.PORT ? process.env.PORT : 3000;
-
-app.listen(port, async () => {
-  await connectDb();
-  console.log(`Server is running on http://localhost:${port}`);
-});
 
 swaggerDocs(app);
 
