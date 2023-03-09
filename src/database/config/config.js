36c233a2ed128112e747ca94527d1317be2dbab7
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2023-03-09 12:23:59 +0200

    fix(docker): solved conflicts

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DEV_DATABASE,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "postgres"
   },
