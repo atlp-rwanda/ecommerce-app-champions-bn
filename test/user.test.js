feed028f1aeba373b11b5085ab47e6169e2c9000
Shumba250 <mudakikwaaimable05@gmail.com>
2023-03-13 12:14:03 +0200

    ft-register-vendor: the administrator should be able to register a new vendor an email containing a rondom password should be sent to the provided email the vendor should be able to signin using the provided email and password appropriate aeeror messages are displayed on unsuccessful attempts

--- test/user.test.js
+++ test/user.test.js
@@ -10,12 +10,34 @@ describe("testing all routes", () => {
 });
 // testing adding a user endpoint
 describe("testing user", () => {
-  test("adding a user", async () => {
+  test("adding a vendor", async () => {
     const response = await request(app).post("/signup").send({
       firstName: "umurungi",
       lastName: "helen",
-      email: "shumba2500@gmail.com"
+      email: "mudakikwaaimable05@gmail.com"
     });
     expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("validations", () => {
+  test("vendor validations", async () => {
+    const response = await request(app).post("/signup").send({
+      firstName: "u",
+      lastName: "helen",
+      email: "mudakikwaaimable05@gmail.com"
+    });
+    expect(response.statusCode).toBe(400);
+  });
+});
+
+describe("routes", () => {
+  test("testing a wrong route", async () => {
+    const response = await request(app).post("/signu").send({
+      firstName: "umurungi",
+      lastName: "helen",
+      email: "mudakikwaaimable05@gmail.com"
+    });
+    expect(response.statusCode).toBe(404);
   });
 });
