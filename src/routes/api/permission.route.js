35e94de32c7ce78a2c11f72285057a11da5d4fe4
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2023-03-14 16:59:06 +0200

    ft(roles-permission): roles and permissions
    - implement roles and permission relationship
    - implement create role and delete role
    - implement create and delete permission
    - implement assignrole and permission to the user
    
    [WIP ft-roles-permission ]

--- src/routes/api/permission.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import Permissions from "../../controllers/permissionController";
-
-
-const permissionRoute = Router();
-
-permissionRoute.post("/permission", Permissions);
-
-export default permissionRoute;
--- src/routes/api/role.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import Roles from "../../controllers/roleController";
-
-
-const roleRoute = Router();
-
-roleRoute.post("/role", Roles);
-
-export default roleRoute;
