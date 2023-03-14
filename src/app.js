d45766e13ed78e82f78993d6907ab70342a194eb
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2023-03-14 19:58:57 +0200

    ft(signin): signin test
    - implement tests
    - implement cookies once login is happening
    
    [finishes ft-siginin

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 /* eslint-disable */
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import indexRouter from "./routes/index";
 
 import morgan from "morgan";
+
 import cookieParser from "cookie-parser";
 
 import languages from './locales/languages';
@@ -13,15 +16,24 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
+<<<<<<< HEAD
 app.use(cookieParser(process.env.JWT_SECRET));
+=======
+app.use(cookieParser(JWT_SECRET));
+>>>>>>> a871efc (ft(signin): signin test)
 app.use(morgan("dev"));
 
 swaggerDocs(app);
 languages(app);
 
 app.get("/", (_, res) => {
-  res.status(200).json("Welcome to our Ecommerce App");
+    res.status(200).json("Welcome to our Ecommerce App");
 });
+<<<<<<< HEAD
 app.use("/", indexRouter);
+=======
+
+app.use(indexRouter);
+>>>>>>> a871efc (ft(signin): signin test)
 
 export default app;
