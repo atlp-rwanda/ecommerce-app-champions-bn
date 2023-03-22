b99c374a16f249f7b848a6e29788a75f5eaa8675
patsicko <patsicko@g>
2023-03-22 13:12:27 +0200

    ft-list-items - Create product model - Create products migration [Finishes#]

---
+++ src/database/migrations/20230322093017-create-product.js
@@ -0,0 +1,70 @@
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      productId: {
+        type: Sequelize.INTEGER,
+        autoIncrement:true,
+        primaryKey:true
+        
+      },
+      vendorId: {
+        type: Sequelize.INTEGER,
+      },
+      productName: {
+        type: Sequelize.STRING,
+        allowNull:false,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+      },
+      productImage:{
+      type: Sequelize.ARRAY(Sequelize.STRING),
+  
+      },
+      productPrice: {
+        type: Sequelize.DOUBLE,
+        allowNull:false,
+      },
+      quantity:{
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        defaultValue:1,
+      },
+      available:{
+        type: Sequelize.BOOLEAN,
+        allowNull:false,
+        defaultValue:false,
+      },
+      productDescription:{
+        type: Sequelize.STRING,
+      },
+      productOwner:{
+       type: Sequelize.STRING,
+       allowNull:false
+      },
+      expiredDate:{
+        type: Sequelize.DATE,
+        allowNull:false
+      }
+      ,
+      expired:{
+      type: Sequelize.BOOLEAN,
+      defaultValue:false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('products');
+  }
+};
