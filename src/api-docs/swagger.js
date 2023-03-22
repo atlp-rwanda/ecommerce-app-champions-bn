57d7d7897cfdb1e46ed97191170cdb76697c79db
Olivier <89464364+oliviertech212@users.noreply.github.com>
2023-03-23 00:37:57 +0200

    ft(social-account)signin with google -solve conflict

--- src/api-docs/swagger.js
+++ src/api-docs/swagger.js
@@ -65,7 +65,7 @@ const options = {
             },
           },
           paths:{
-            ...userRouteDoc,
+            // ...userRouteDoc,
             ...vendorRouteDoc,
             ...GoogleaouthDocs,
             ...permissionRouteDoc,
