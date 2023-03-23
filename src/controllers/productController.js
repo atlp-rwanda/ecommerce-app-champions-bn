2e9c861cd049bde858d0278e5027f2a01160b5ad
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2023-03-23 17:15:27 +0200

    ft(productSearch): searching product
    - implement search functionality
    
    [WIP ft-productSearch ]

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -65,6 +65,22 @@ class productController {
     }
   }
 
+  static async searchProduct(req,res){
+    const { searchParam } = req.query;
+    try {
+        const search = await Product.findAll({where:{
+            [Op.or]:[
+                {productName:{[Op.iLike]:`%${searchParam}%`}},
+                {productDescription:{[Op.iLike]:`%${searchParam}%`}},
+                {productPrice:{[Op.iLike]:`%${searchParam}%`}},
+                {productOwner:{[Op.iLike]:`%${searchParam}%`}},
+            ]
+        }});
+        return res.status(200).json({status:"success",data:search});
+    } catch (error) {
+        return res.status(500).json({status:"error",error:error.message});
+    }
+}
 
   static async availableProducts(req,res){
     try {
--- src/database/models/product.js
+++ src/database/models/product.js
@@ -1,8 +1,4 @@
-
-
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
