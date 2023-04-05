2db7379ff7370250daedd568949fe502c59540a1
Shumba250 <mudakikwaaimable05@gmail.com>
2023-04-05 16:01:35 +0200

    ft-update-cart Create a PUT endpiont for updating quantity of items in the cart Create a feature to calculate new total Create a feature to remove a product from the cart if it's quantity is zero Create a feature for checking if a product is in the cart Create a feature for checking if the quantity updated is in stock Create a swagger documentation [FINISHES #26]

---
+++ src/routes/cart.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import CartController from "../controllers/cartController";
+import { verifyBuyer } from "../middlewares/authenticate";
+import updateCartSchema from "../validations/updateCartValidation";
+import { validate } from "../middlewares/validate";
+
+const cartRoute = express.Router();
+
+cartRoute.post("/add/:productId", verifyBuyer, CartController.addItem);
+cartRoute.put("/updateCart/:productId",verifyBuyer,validate(updateCartSchema),CartController.updateCart);
+cartRoute.get("/getAll", verifyBuyer, CartController.getCartItems);
+
+export default cartRoute;
