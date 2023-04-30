621f8932040d51371ca3ecf7640fa7063f1bb9af
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2023-04-30 21:37:04 +0200

    fix(update-profile) : fix update buyer profile
    - fix userId to UserId in update profile of Buyer
    
    [Finishes fix-update-buyer-profile ]

--- src/routes/buyer.route.js
+++ src/routes/buyer.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import BuyerController from "../controllers/buyerController";
 import { validate } from "../middlewares/validate";
 import { buyerSchema, buyerUpSchema } from "../validations/buyerValidationSchema";
-import isLoggedIn from "../middlewares/checklogin";
+import { verifyBuyer } from "../middlewares/authenticate";
 
 
 const buyerRoute = Router();
@@ -10,6 +10,6 @@ const buyerRoute = Router();
 buyerRoute.post("/signup", validate(buyerSchema), BuyerController.createBuyer);
 buyerRoute.get("/verifyToken", BuyerController.verifyBuyer);
 buyerRoute.get("/oneProfile/:userId", BuyerController.getProfile);
-buyerRoute.put("/profile/:userId", validate(buyerUpSchema), isLoggedIn, BuyerController.updateProfile);
+buyerRoute.put("/profile/:userId", validate(buyerUpSchema), verifyBuyer, BuyerController.updateProfile);
 
 export default buyerRoute;
